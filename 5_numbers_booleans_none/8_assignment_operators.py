8f51bd14c3d37ad12a21896b73160d1d9ff01dd3
rubengr16 <ruben16gallego@gmail.com>
2020-12-30 01:43:10 +0100

    none

--- 5_numbers_booleans_none/8_assignment_operators.py
+++ 5_numbers_booleans_none/8_assignment_operators.py
@@ -1,3 +1,25 @@
-# we have seen = as assingment operator, but there are several more called compound operators
+# We have seen = as assingment operator, but there are several more called compound operators
 # they combine together a numeric operation with the assignment operator
+x = 3
+print('x =', x)
 
+x += 2  # + asignment adds the variable plus the value
+print('x += 2 == x = x + 2 => x =', x)
+
+x -= 2  # - asignment substracts the value to the variable
+print('x -= 2 == x = x - 2 => x =', x)
+
+x *= 12  # * asignment multiplies the variable by the value
+print('x *= 12 == x = x * 12 => x =', x)
+
+x /= 4  # / asignment divides the variable by the value
+print('x /= 4 == x = x / 4 => x =', x)
+
+x //= 2  # // asignment divides to floor the variable by the value
+print('x //= 2 == x = x // 2=> x =', x)
+
+x %= 1.6  # % asignment gives the modulus (reminder) of dividing the variable by the value
+print('x %= 1.6 == x = x % 1.6 => x =', x)
+
+x **= 2  # ** asignment raises the variable to the value
+print('x **= 2 == x = x ** 2 => x =', x)
---
+++ 5_numbers_booleans_none/9_none.py
@@ -0,0 +1,12 @@
+# NoneType has a single value: None
+# it is used to create a variable with no initial value for it
+none = None  # None assignment
+
+print('none value =', none)
+print('none type =', type(none))
+
+# Using is and is not we can ask for the presence of None having as result a boolean
+
+print('none is None?:', none is None)
+print('"notNone" is None?:', 'notNone' is None)
+print('"notNone" is not None?:', 'notNone' is not None)
