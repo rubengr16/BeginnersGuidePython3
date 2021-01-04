8a7b9ef4cec05670b1f47b6b2f9321bbd2618027
rubengr16 <ruben16gallego@gmail.com>
2021-01-04 23:36:35 +0100

    loops started

---
+++ 7_loops/1_while.py
@@ -0,0 +1,14 @@
+# The while loop repeats a block of code while a test condition is True,
+# it is used when the number of iterations (repetitions) is unknown,
+# the test of the condition is made before each iteration,
+# it has the form: while test_condition_is_true:
+#                       block_of_code
+# Note: indentation is important
+print('Enter values to be powered by themselves, 0 to stop')
+num = int(input('Enter a value: '))
+
+while(num != 0):  # num != 0 is the test condiition
+    print(num, 'squared is', num * num)
+    num = int(input('Enter a value: '))
+
+print('Done')  # Statement out of the while
